aecaa6751c0665f228a582ceca903c43600e0bf3
ariyannandi <ariyannandi2711@gmail.com>
2023-12-02 03:11:37 +0530

    day 1 part 2 done

--- day-1/day-1.js
+++ day-1/day-1.js
@@ -2,19 +2,51 @@ import fs from "fs";
 
 const inputText = "input.txt";
 
+const wordDigits = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+};
+
+const getRowDigit = (string) => {
+  const digitsArr = [];
+
+  for (let i = 0; i < string.length - 1; i++) {
+    const word = string
+      .slice(i)
+      .match(/(\d|one|two|three|four|five|six|seven|eight|nine)/g);
+    if (word) {
+      digitsArr.push(...word);
+    }
+  }
+
+  const digits = digitsArr.map((matchedDigit) => {
+    const digit = wordDigits[matchedDigit] || parseInt(matchedDigit, 10);
+    return isNaN(digit) ? null : digit;
+  });
+
+  return digits.filter((digit) => digit !== null);
+};
+
 const sumOfValues = (input) => {
   let sum = 0;
 
   for (let i of input) {
-    const digitsArr = i.match(/\d/g);
+    const digitsArr = getRowDigit(i);
 
     if (digitsArr && digitsArr.length >= 1) {
-      const digits = digitsArr.map((digit) => parseInt(digit));
-      const digit1 = digits[0];
-      const digit2 = digits.length === 1 ? digit1 : digits[digits.length - 1];
+      const firstDigit = digitsArr[0];
+      const lastDigit = digitsArr[digitsArr.length - 1];
 
+      const digit1 = parseInt(firstDigit);
+      const digit2 = parseInt(lastDigit);
       const lineValue = digit1 * 10 + digit2;
-
       sum += lineValue;
     }
   }
